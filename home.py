fcb21a12389d00bce213da6793818a4c2c32c3db
A-Red-Hen <99688148+A-Red-Hen@users.noreply.github.com>
2024-03-28 03:22:18 +0000

    added gpkg files and home.py

---
+++ home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+image_address = "https://media.wired.com/photos/64e56cb3ff65ec99d0acf97f/master/pass/wildfire-canada-science-GettyImages-1607866119.jpg"
+st.title('<Project Name>')
+st.image(image_address, caption="<caption>")
